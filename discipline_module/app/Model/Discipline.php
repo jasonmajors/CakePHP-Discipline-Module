d73ddd71eef1744a7a4afb37105270849e9f7683
Jason <jasonrmajors@gmail.com>
2014-11-05 01:34:47 -0800

    Made the form fields for new employee and new disciplines required.

--- discipline_module/app/Model/Discipline.php
+++ discipline_module/app/Model/Discipline.php
@@ -1,5 +1,14 @@
 <?php
 class Discipline extends AppModel {
 	public $belongsTo = 'Employee';
+	
+	public $validate = array(
+		'type' => array(
+			'rule' => 'notEmpty'
+		),
+		'description' => array(
+			'rule' => 'notEmpty'
+		),
+	);
 }
 ?>
--- discipline_module/app/Model/Employee.php
+++ discipline_module/app/Model/Employee.php
@@ -1,5 +1,17 @@
 <?php 
 class Employee extends AppModel {
 	public $hasMany = 'Discipline';
+	
+	public $validate = array(
+		'firstn' => array(
+			'rule' => 'notEmpty'
+		),
+		'lastn' => array(
+			'rule' => 'notEmpty'
+		),
+		'department' => array(
+			'rule' => 'notEmpty'
+		)
+	);
 }
 ?>
